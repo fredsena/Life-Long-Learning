f9da329d08054ddb184bcda088e7f56e8e6720df
Fred Sena <sena.fred@gmail.com>
2019-06-19 23:27:13 +0100

    Create Close_Fred_Apps.bat

---
+++ Close_Fred_Apps.bat
@@ -0,0 +1,16 @@
+
+@echo off
+
+taskkill /IM notepad++.exe
+
+taskkill /IM OUTLOOK.EXE >nul
+
+taskkill /IM slack.exe >nul
+
+taskkill /IM chrome.exe >nul
+
+taskkill /IM Cmder.exe 
+
+taskkill /IM devenv.exe
+
+exit
