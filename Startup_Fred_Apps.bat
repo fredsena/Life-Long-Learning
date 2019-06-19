fc73543f1656bff0b6d97eb2243a731223f580eb
Fred Sena <sena.fred@gmail.com>
2019-06-19 23:26:11 +0100

    Create Startup_Fred_Apps.bat

---
+++ Startup_Fred_Apps.bat
@@ -0,0 +1,21 @@
+@echo off
+
+cd "C:\Program Files\Notepad++"
+start notepad++.exe
+
+cd "C:\Program Files\Microsoft Office\root\Office16"
+start OUTLOOK.EXE
+
+cd C:\Users\fred_sena\AppData\Local\slack
+start slack.exe
+
+cd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe
+
+cd "C:\Users\fred_sena\Install\cmder"
+start Cmder.exe
+
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE"
+start devenv.exe
+
+exit
